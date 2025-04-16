42ff902121944b833f8c11fa7a585ba47f4b9429
Jeff Patterson <jeff_patterson@outlook.com>
2025-04-16 06:02:13 -0500

    Section Complete

--- ARDicee/ViewController.swift
+++ ARDicee/ViewController.swift
@@ -23,50 +23,11 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         /// locating a plane in the real world.
         //self.sceneView.debugOptions = [ARSCNDebugOptions.showFeaturePoints /*, ARSCNDebugOptions.showWorldOrigin*/]
         
-        // Set the view's delegate
+        /// Set the view's delegate
         sceneView.delegate = self
-        
-        /// Create a new scene that is in fact, a simple cube
-        //let cube = SCNBox(width: 0.1, height: 0.1, length: 0.1, chamferRadius: 0.01)
-        //let sphere = SCNSphere(radius: 0.2)
-        
-        /// Material is the surface of the object.
-        //let material = SCNMaterial()
-        //material.diffuse.contents = UIColor.red
-        //material.diffuse.contents = UIImage(named: "art.scnassets/moon.jpg")
-        
-        /// Attach the material to the cube.
-        //cube.materials = [material]
-        //sphere.materials = [material]
-        
-        /// Node is a position in 3D space.
-        //let node = SCNNode(geometry: cube)
-        //let node = SCNNode()
-        
-        /// Postion of the object in 3D space. This is centered, low and further back a little.
-        //node.position = SCNVector3(0, 0.1, -0.5)
-        
-        /// With a node in space we now assign the position to the cube.
-        //node.geometry = cube
-        //node.geometry = sphere
-        
-        /// put the node into the scene
-        //sceneView.scene.rootNode.addChildNode(node)
         
         /// Add some depth as shadow to the object
         sceneView.autoenablesDefaultLighting = true
-        
-        // Create a new scene
-        //        let diceScene = SCNScene(named: "art.scnassets/diceCollada.scn")!
-        //        if let diceNode = diceScene.rootNode.childNode(withName: "Dice", recursively: true) {
-        //            diceNode.position = SCNVector3(0, 0, -0.1)
-        //
-        //            // Set the scene to the view
-        //            sceneView.scene.rootNode.addChildNode(diceNode)
-        //            //sceneView.scene = scene
-        //        }
-        
-        
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -91,49 +52,48 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         sceneView.session.pause()
     }
     
+    //MARK: - Touch Methods
+    
     /// This method will convert touches on the screen to real world coordinates via SceneKit and AR
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         /// Multitouch is not enabled (we don't want it) so we can assume there is only one touch and we
         /// will capture it.
         if let touch = touches.first {
             let touchLocation = touch.location(in: sceneView)
-            
             /// This will convert the touch location on the screen into a 3D point in the real world.
             /// The hitTest will return an array of SCNHitTestResult objects.
             /// The .existingPlaneUsingExtent that is already in the scene becasue we put it on in the
             /// delegate method func renderer(...
             let results = sceneView.hitTest(touchLocation, types: .existingPlaneUsingExtent)
-            
             if let hitResult = results.first {
-                //print("hitResult: \(hitResult)")
-                
-                /// Create a new scene
-                let diceScene = SCNScene(named: "art.scnassets/diceCollada.scn")!
-                if let diceNode = diceScene.rootNode.childNode(withName: "Dice", recursively: true) {
-                    diceNode.scale = SCNVector3(3.0, 3.0, 3.0)
-                    
-                    diceNode.position = SCNVector3(
-                        x: hitResult.worldTransform.columns.3.x,
-                        y: hitResult.worldTransform.columns.3.y + diceNode.boundingSphere.radius * 3.0,
-                        z: hitResult.worldTransform.columns.3.z)
-                    
-                    diceArray.append(diceNode)
-                    // Set the scene to the view
-                    sceneView.scene.rootNode.addChildNode(diceNode)
-                    roll(dice: diceNode)
-                }
+                addDice(atLocation: hitResult)
             }
+        }
+    }
+
+
+    //MARK: - Dice Methods
+    
+    func addDice(atLocation location: ARHitTestResult) {
+        //print("hitResult: \(hitResult)")
+        
+        /// Create a new scene
+        let diceScene = SCNScene(named: "art.scnassets/diceCollada.scn")!
+        if let diceNode = diceScene.rootNode.childNode(withName: "Dice", recursively: true) {
+            diceNode.scale = SCNVector3(3.0, 3.0, 3.0)
+            
+            diceNode.position = SCNVector3(
+                x: location.worldTransform.columns.3.x,
+                y: location.worldTransform.columns.3.y + diceNode.boundingSphere.radius * 3.0,
+                z: location.worldTransform.columns.3.z)
+            
+            diceArray.append(diceNode)
+            // Set the scene to the view
+            sceneView.scene.rootNode.addChildNode(diceNode)
+            roll(dice: diceNode)
         }
     }
     
-    /// Role all of the dice
-    func rollAll() {
-        if !diceArray.isEmpty {
-            for dice in diceArray {
-                roll(dice: dice)
-            }
-        }
-    }
     
     func roll(dice: SCNNode) {
         /// Create a random result between 1 and 4 that represents the die face in the x
@@ -149,7 +109,15 @@ class ViewController: UIViewController, ARSCNViewDelegate {
             duration: 0.5))
     }
     
-
+    /// Role all of the dice
+    func rollAll() {
+        if !diceArray.isEmpty {
+            for dice in diceArray {
+                roll(dice: dice)
+            }
+        }
+    }
+    
     
     @IBAction func rollAgain(_ sender: UIBarButtonItem) {
         rollAll()
@@ -167,37 +135,45 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         }
     }
 
+    //MARK: - ARSCNViewDelegate Methods
+    
     /// This will search for the Horizontal plane that can be used as an  Anchor of the object to the plane
     /// IRL of the scene.
     func renderer(_ renderer: any SCNSceneRenderer, didAdd node: SCNNode, for anchor: ARAnchor) {
-        if anchor is ARPlaneAnchor {
-            /// When a plane is detected, cast it as an ARPlanAnchor to use it as a reference.
-            let planeAnchor = anchor as! ARPlaneAnchor
-            
-            let plane = SCNPlane(width: CGFloat(planeAnchor.planeExtent.width),
-                                 height: CGFloat(planeAnchor.planeExtent.height))
-            
-            let planeNode = SCNNode()
-            /// This planeNode creates a 2D plane in the vertical orientation with no Z axis.
-            planeNode.position = SCNVector3(planeAnchor.center.x, 0, planeAnchor.center.z)
-            
-            /// We need the planeNode horizontal so we need to transform it and rotate it 90°. The
-            /// method needs radians. 180° = 1π radians. 90° = π/2 radians and is measured CCW
-            /// from the position. We want to go CW so we need to negate the value.
-            planeNode.transform = SCNMatrix4MakeRotation((-Float.pi/2), 1, 0, 0)
-            
-            //let gridMaterial = SCNMaterial()
-            let transparentMaterial = SCNMaterial()
-            //gridMaterial.diffuse.contents = UIImage(named: "art.scnassets/grid.png")
-            transparentMaterial.diffuse.contents = UIColor.clear
-            //plane.materials = [gridMaterial]
-            plane.materials = [transparentMaterial]
-            planeNode.geometry = plane
-            //sceneView.scene.rootNode.addChildNode(planeNode)
-            node.addChildNode(planeNode)
-            
-        } else {
-            return
-        }
+        guard let planeAnchor = anchor as? ARPlaneAnchor else { return }
+        
+        let planeNode = createPlane(withPlaneAnchor: planeAnchor)
+
+        node.addChildNode(planeNode)
     }
+    
+    //MARK: - Plane rederring methods
+    
+    func createPlane(withPlaneAnchor planeAnchor: ARPlaneAnchor) -> SCNNode {
+        
+        let plane = SCNPlane(width: CGFloat(planeAnchor.planeExtent.width),
+                             height: CGFloat(planeAnchor.planeExtent.height))
+        
+        let planeNode = SCNNode()
+        /// This planeNode creates a 2D plane in the vertical orientation with no Z axis.
+        planeNode.position = SCNVector3(planeAnchor.center.x, 0, planeAnchor.center.z)
+        
+        /// We need the planeNode horizontal so we need to transform it and rotate it 90°. The
+        /// method needs radians. 180° = 1π radians. 90° = π/2 radians and is measured CCW
+        /// from the position. We want to go CW so we need to negate the value.
+        planeNode.transform = SCNMatrix4MakeRotation((-Float.pi/2), 1, 0, 0)
+        
+        //let gridMaterial = SCNMaterial()
+        //gridMaterial.diffuse.contents = UIImage(named: "art.scnassets/grid.png")
+        //plane.materials = [gridMaterial]
+        let transparentMaterial = SCNMaterial()
+        transparentMaterial.diffuse.contents = UIColor.clear
+        plane.materials = [transparentMaterial]
+        
+        planeNode.geometry = plane
+        return planeNode
+    }
+        
 }
+
+
